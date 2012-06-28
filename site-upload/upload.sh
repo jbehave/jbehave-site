51acc5adff94ab93e5c55a84e9136ad06a4505cb
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-06-28 16:33:29 +0200

    Added scripts for uploading all download pages.

--- site-upload/upload.sh
+++ site-upload/upload.sh
@@ -1,9 +1,12 @@
 
 ./upload-reference.sh jbehave-site 3.2-SNAPSHOT latest
-./upload-reference.sh jbehave 3.6.7 stable
 ./upload-reference.sh jbehave 3.7-SNAPSHOT latest
-./upload-reference.sh jbehave-web 3.5.3 stable
 ./upload-reference.sh jbehave-web 3.6-SNAPSHOT latest
 
+./upload-reference.sh jbehave 3.6.7 stable
+./upload-reference.sh jbehave-web 3.5.3 stable
+
+./upload-download.sh jbehave 3.6.7 stable
+./upload-download.sh jbehave-web 3.5.3 stable
 
 
