b0b42edd86694313a3f9a9981002433ea50bfc7b
Mauro Talevi <mauro.talevi@aquilonia.org>
2014-03-08 19:29:22 +0100

    Updated upload versions.

--- site-upload/upload.sh
+++ site-upload/upload.sh
@@ -1,12 +1,16 @@
 
 ./upload-reference.sh jbehave-site 3.2-SNAPSHOT latest
-./upload-reference.sh jbehave 3.7-SNAPSHOT latest
+./upload-reference.sh jbehave 3.10-SNAPSHOT latest
 ./upload-reference.sh jbehave-web 3.6-SNAPSHOT latest
 
-./upload-reference.sh jbehave 3.6.7 stable
-./upload-reference.sh jbehave-web 3.5.3 stable
+./upload-reference.sh jbehave 3.9.1 stable
+./upload-reference.sh jbehave 4.0-beta-5 preview
+./upload-reference.sh jbehave-web 3.5.5 stable
+./upload-reference.sh jbehave-web 3.6-beta-2 preview
 
-./upload-download.sh jbehave 3.6.7 stable
-./upload-download.sh jbehave-web 3.5.3 stable
+./upload-download.sh jbehave 3.9.1 stable
+./upload-download.sh jbehave 4.0-beta-5 preview
+./upload-download.sh jbehave-web 3.5.5 stable
+./upload-download.sh jbehave-web 3.6-beta-2 preview
 
 
